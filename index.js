566bce4f33e9c7cf52ed7643030cf0ceb64c2091
Farid Alam <faridalam62@gmail.com>
2023-05-01 01:19:38 +0600

    Updated

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const categories = require('./categories.json')
 const bookings = require('./bookings.json')
+const cors = require('cors')
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.get('/',(req,res)=>{
  res.send('Server is running')
 })
